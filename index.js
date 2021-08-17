326645e6b6e258d5fecbaac5b73a0ca491fcba1f
William Mosto <will.mosto@gmail.com>
2021-08-17 09:06:47 -0400

    commit tree 0523194a049dc38a0a8ab6712754862d337353a8

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  fetch('https://anapioficeandfire.com/api/books')
+  .then(res => res.json())
+  .then(json => renderBooks(json))
+  return fetch('https://anapioficeandfire.com/api/books')
 }
 
 function renderBooks(books) {
@@ -12,6 +15,4 @@ function renderBooks(books) {
   });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
-});
+document.addEventListener('DOMContentLoaded', () => fetchBooks())
